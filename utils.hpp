66d87c7c9f7307391d06b784bf0bb81a8b1121a9
咚咚 <lf2kexe@gmail.com>
2018-09-30 16:24:12 +0800

    add explode() in utils.hpp

--- utils.hpp
+++ utils.hpp
@@ -3,6 +3,19 @@
 using namespace eosio;
 using namespace std;
 
+const vector<string> explode(const string& s, const char& c) {
+    string buff{""};
+    vector<string> v;
+
+    for(auto n:s) {
+        if(n != c) buff+=n; 
+        elseif(n == c && buff != "") { v.push_back(buff); buff = ""; }
+    }
+    if(buff != "") v.push_back(buff);
+
+    return v;
+}
+
 uint64_t string_to_price(string s) {
     uint64_t z = 0;
     for (int i=0;i<s.size();++i) {
